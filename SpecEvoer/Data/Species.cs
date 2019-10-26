1d8539693534380fed4079daf8d0267ed58ce6d6
BrettTeets <Brett.teets@gmail.com>
2019-10-26 13:15:20 -0500

    Laid the ground work.

---
+++ SpecEvoer/Data/Species.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecEvoer.Data
+{
+    public class Species : IDescribing
+    {
+        public Era StartEra { get; set; }
+        public Era EndEra { get; set; }
+        public Cateogry Cateogry { get; set; }
+        public Species Parent { get; set; }
+        public List<Species> Decendants { get; set; }
+
+
+        #region Interfaces
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Keywords { get; set; }
+        #endregion
+    }
+
+    public class Cateogry : IDescribing
+    {
+        public Era StartEra { get; set; }
+
+        public Cateogry Parent { get; set; }
+        public List<Cateogry> Decendants { get; set; }
+        public List<Species> Members { get; set; }
+        #region Interfaces
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Keywords { get; set; }
+        #endregion
+    }
+
+    public class Era
+    {
+        public string name { get; set; }
+        public int StartYear { get; set; }
+        public int EndYear { get; set; }
+    }
+
+    public class Biosphere
+    {
+        public List<Era> Eras = new List<Era>();
+        public List<Cateogry> Cateogries = new List<Cateogry>();
+        public List<Species> Species = new List<Species>();
+    }
+
+
+
+
+
+
+    
+
+    interface IDescribing
+    {
+        string Name { get; set; }
+        string Description { get; set; }
+        string Keywords { get; set; }
+    }
+
+
+}
---
+++ SpecEvoer/Logic/Specer.cs
@@ -0,0 +1,34 @@
+﻿using SpecEvoer.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecEvoer.Logic
+{
+    class Specer
+    {
+        Biosphere Biosphere;
+
+        public void CreateNewBiosphere()
+        {
+            Biosphere = new Biosphere();
+        }
+
+        public void CreateNewEra()
+        {
+
+        }
+
+        public void CreateNewCategory()
+        {
+
+        }
+
+        public void CreateNewSpecies()
+        {
+
+        }
+    }
+}
